cd03e54b627d224d54a9a60cb9d34204a103eeab
manvy009 <manvy009@gmail.com>
2026-01-28 23:33:12 +0530

    Time: 3 ms (51.48%), Space: 20.6 MB (17.06%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,14 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        nwi=[(num,i)for i,num in enumerate(nums)]
-        nwi.sort(key=lambda x:x[0])
-        l,r=0,len(nums)-1
-        while l<r:
-            s=nwi[l][0]+nwi[r][0]
-            if s==target:
-                return [nwi[l][1], nwi[r][1]]
-            elif s<target:
-                l+=1
-            else:
-                r-=1
-        return []
+        seen={}
+        for i,num in enumerate(nums):
+            needed=target-num
+            if needed in seen:
+                return[seen[needed],i]
+            seen[num]=i
