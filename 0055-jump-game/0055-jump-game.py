d04e9330ea080ef4ba14415ee86b9072b5c21656
manvy009 <manvy009@gmail.com>
2026-02-21 18:38:56 +0530

    Time: 14 ms (81.12%), Space: 20.3 MB (54.59%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,11 +1,10 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        max_reach = 0  
-
-        for i in range(len(nums)):
-            if i > max_reach:  
-                return False  
-            max_reach = max(max_reach, i + nums[i])  
-            if max_reach >= len(nums) - 1:
-                return True 
-        return False
+        fuel=0
+        for n in nums:
+            if fuel<0:
+                return False
+            elif fuel<n:
+                fuel=n
+            fuel-=1
+        return True
