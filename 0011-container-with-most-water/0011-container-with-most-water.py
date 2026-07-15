98c455e34aa696f982db9f414f98eee571398f9d
manvy009 <manvy009@gmail.com>
2026-07-16 00:07:11 +0530

    Time: 62 ms (30.45%), Space: 29.7 MB (13.93%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -10,3 +10,7 @@ class Solution:
             else:
                 j-=1
         return res
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
